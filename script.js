9691489b0be8c168c56cc3753a5cf43981f35766
jillwilliams <codeGeek.03.14@gmail.com>
2019-10-26 11:58:41 -0700

    done

---
+++ script.js
@@ -0,0 +1,40 @@
+// const example1 = () => {
+//     var a = document.getElementById("ex1answer").value;
+//     if (a == 6) {
+//         return "Good Job!"
+//     } else
+//         return "Try Again."
+// };
+// const ex1solution = ( ) => {
+//     document.getElementById("demo").innerHTML=example1();
+// }
+// const example2 = () => {
+//     var a = document.getElementById("ex2answer").value;
+//     if (a == 2) {
+//         return "Good Job!"
+//     } else
+//         return "Try Again."
+// };
+// const ex2solution = ( ) => {
+//     document.getElementById("demo2").innerHTML=example2();
+// }
+// const example3 = () => {
+//     var a = document.getElementById("ex3answer").value;
+//     if (a == 3) {
+//         return "Good Job!"
+//     } else
+//         return "Try Again."
+// };
+// const ex3solution = ( ) => {
+//     document.getElementById("demo3").innerHTML=example3();
+// }
+// const example4 = () => {
+//     var a = document.getElementById("ex4answer").value;
+//     if (a == 14) {
+//         return "Good Job!"
+//     } else
+//         return "Try Again."
+// };
+// const ex4solution = ( ) => {
+//     document.getElementById("demo4").innerHTML=example4();
+// }
